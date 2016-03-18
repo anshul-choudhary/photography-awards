e009dc18c73eeb7842092912bee8aa1a7b46aa25
Anshul Choudhary <anshul@Anshuls-MacBook-Pro.local>
2016-03-18 11:02:22 +0530

    awards project added

---
+++ awards/awards/local_settings.py
@@ -0,0 +1,17 @@
+
+
+EMAIL_SITE_URL = 'localhost:8000'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'awards',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'autocommit': True, }
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
