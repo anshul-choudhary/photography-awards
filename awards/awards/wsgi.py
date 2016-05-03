093a94cefcf1894326ed95c344815fddb482f1eb
Anshul Choudhary <anshul@Anshuls-MacBook-Pro.local>
2016-05-03 12:58:00 +0530

    index page

--- awards/awards/wsgi.py
+++ awards/awards/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for awards project.
+WSGI config for xcellinsproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
+
+site.addsitedir('/root/.virtualenvs/awards/lib/python2.7/site-packages')
+sys.path.append('/var/www/awards')
+sys.path.append('/root/.virtualenvs/awards/lib/python2.7/site-packages')
+sys.path.append('/root/.virtualenvs/awards/lib/python2.7/site-packages/django/contrib/admindocs')
+sys.path.append('/root/.virtualenvs/awards/lib/python2.7/site-packages/django')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'awards.settings'
+
+activate_env=os.path.expanduser(
+    "/root/.virtualenvs/awards/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "awards.settings")
-
 application = get_wsgi_application()
